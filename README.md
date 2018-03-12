d5ca674c91c514d3a2a239378d2402635305f15d
Cédric <nope-mcnopeface@asdf.org>
2018-03-12 11:58:15 +0100

    Hannnnnnnnds

--- README.md
+++ README.md
@@ -3,11 +3,11 @@ hadronox
 
 I can't be bothered to write a comprehensive understandable documentation. For now, there's a brief description of the configuration file syntax, and an example. Hopefully i'll be able to make this thing work next time i need it.
 
-# Configuration file
+# Command line arguments
 
-The configuration file tells `hadronox` wtf it should do:
+Just type:
 ```
-    hadronox -c path/to/stuff/config.json
+hadronox.py --help
 ```
 
 It is written in JSON.
@@ -16,24 +16,26 @@ It is written in JSON.
 
 Contains all metadata.
 
-## `language`
-
-Translations.
-
 ## `targets`
 
 All paths are relative to the config file.
 
-* `name`: For log purposes.
+* `name`: Name of the target.
+* `type`: See below.
+
+### Target type `template`
+
 * `template`: Path to template file.
 * `input`: Path to the input `csv` file.
 * `output`: Path to the output document.
 * `sort`: List of sort columns (of the input `csv` file).
-* `groups`: List of groups descriptors.
+* `groups`: List of groups descriptors, see after.
 
-### Group descriptors
-
-A `csv` file is a list of row. This list can be divided into disjoint sublist according to the specifications of a group descriptor.
+Group descriptors: A `csv` file is a list of row. This list can be divided into disjoint sublist according to the specifications of a group descriptor.
 
 * `column`: Name of the separating column, i.e. that from which the rows are regrouped by value.
 * `additionalColumns`: Each group can have additional columns whose value is defined by that of the separating column. See example.
+
+### Target type `email`
+
+Doing nothing for now.
--- hadronox.py
+++ hadronox.py
@@ -8,70 +8,93 @@ import os
 from relatorio.templates.opendocument import Template
 
 
-def generateTarget(target, config):
-    
-    print("Generating target " + target["name"] + "... ", end = "")
+class AbstractTarget(dict):
 
-    raw = []
-    with open(target["input"]) as csvfile:
-        raw = sorted(list(csv.DictReader(csvfile)), key = lambda x : sortKey(target, x))
-    data = makeGroups(raw, target.get("groups", []))
-
-    with open(target["output"], "wb") as outfile:
-        template = Template(source = '', filepath = target["template"])
-        doc = template.generate(data = data, meta = config["meta"],
-                                language = config["language"])
-        outfile.write(doc.render().getvalue())
-
-    print("Done")
+    def make(self, metadatas):
+        pass
 
 
-def sortKey(target, x):
-    res = ""
-    for k in target.get("sort", []):
-        res += x[k]
-    return res
+class TemplateTarget(AbstractTarget):
+
+    def make(self, metadatas):
+        raw = []
+        with open(self["input"]) as csvfile:
+            raw = sorted(list(csv.DictReader(csvfile)),
+                         key = lambda x : self._sortKey(x))
+        data = self._makeGroups(raw, self.get("groups", []))
+        with open(self["output"], "wb") as outfile:
+            template = Template(source = '', filepath = self["template"])
+            doc = template.generate(data = data, meta = metadatas)
+            outfile.write(doc.render().getvalue())
+
+    def _makeGroups(self, ungroupedData, sepList):
+        if len(sepList) == 0:
+            return ungroupedData
+        else:
+            sep = sepList[0]
+            groupedDict = {}
+            for r in ungroupedData:
+                val = r[sep["column"]]
+                if val not in groupedDict.keys():
+                    groupedDict[val] = []
+                groupedDict[val] += [r]
+            data = []
+            for k in sorted(groupedDict.keys()):
+                group = {
+                    "value" : k,
+                    "data"  : self._makeGroups(groupedDict[k], sepList[1:])
+                }
+                additionalColumns = sep.get("additionalColumns", {})
+                for a in additionalColumns.keys():
+                    group[a] = additionalColumns[a].get(k, "??") 
+                data += [group]
+            return { sep["column"] + "_list" : data }
+
+    def _sortKey(self, x):
+        res = ""
+        for k in self.get("sort", []):
+            res += x[k]
+        return res
 
 
-def makeGroups(l, sepList):
+class EmailTarget(AbstractTarget):
 
-    if len(sepList) == 0:
-        return l
+    def make(self, metadatas):
+        print("Lololo")
 
-    else:
 
-        sep = sepList[0]
-
-        groupedDict = {}
-        for r in l:
-            val = r[sep["column"]]
-            if val not in groupedDict.keys():
-                groupedDict[val] = []
-            groupedDict[val] += [r]
-
-        data = []
-        for k in sorted(groupedDict.keys()):
-            group = {
-                "value" : k,
-                "data"  : makeGroups(groupedDict[k], sepList[1:])
-            }
-            additionalColumns = sep.get("additionalColumns", {})
-            for a in additionalColumns.keys():
-                group[a] = additionalColumns[a].get(k, "??") 
-            data += [group]
-
-        return { sep["column"] + "_list" : data }
-
+targetTypes = {"email"    : EmailTarget,
+               "template" : TemplateTarget}
 
 if __name__ == "__main__":  
     
     parser = optparse.OptionParser()
     parser.add_option("-c", "--config", action = "store", dest = "config",
                       type = "string", help = "Configuration file")
+    parser.add_option("-t", "--targets", action = "store", dest = "targets",
+                      type = "string",
+                      help = "Targets to execute, comma-separated",
+                      default = "all")
+    parser.add_option("-l", "--list-targets", action = "store_true",
+                      dest = "listTargets", default = "false",
+                      help = "List available targets")
     (options, args) = parser.parse_args()
 
     with open(options.config, 'r') as configfile:
         os.chdir(os.path.dirname(options.config))
         config = json.loads(configfile.read())
-        for target in config["targets"]:
-            generateTarget(target, config)
+        targetMap = {}
+        for t in config["targets"]:
+            targetMap[t["name"]] = targetTypes[t["type"]](t)
+        if options.listTargets:
+            print("Available targets: ", sorted(targetMap.keys()))
+        else: 
+            targets = []
+            if options.targets == "all":
+                targets = targetMap.keys()
+            else:
+                targets = options.targets.split(",")
+            for k in targets:
+                print("Making target " + k + " ... ", end = "")
+                targetMap[k].make(config["meta"])
+                print("Done")
